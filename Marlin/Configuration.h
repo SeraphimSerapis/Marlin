c250715971f950607bf6488af0535bc2618c9e6b
Tim Messerschmidt <timmesserschmidt@gmail.com>
2020-07-09 11:37:22 +0200

    Adjusted to new bed leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -975,7 +975,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44.5, -10, -2.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -44.5, -10, -2.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
