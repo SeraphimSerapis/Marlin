0f8acd4301b864ac42a0eab48d476ba9e5cc1466
Tim Messerschmidt <timmesserschmidt@gmail.com>
2020-07-24 09:03:50 +0200

    PID Autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,9 +491,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 29.72
-  #define DEFAULT_Ki 2.45
-  #define DEFAULT_Kd 89.93
+  #define DEFAULT_Kp 28.89
+  #define DEFAULT_Ki 2.34
+  #define DEFAULT_Kd 89.16
 
 
 #endif // PIDTEMP
@@ -983,7 +983,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 15
+#define PROBING_MARGIN 17
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 10000
